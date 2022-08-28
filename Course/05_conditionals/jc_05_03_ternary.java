eee3b3ed5f724a0a10507fb85d187aa28a369f45
Arijit <arijitdasofficial@gmail.com>
2022-08-28 18:44:53 +0530

    improved ternary

--- Course/05_conditionals/jc_05_03_ternary.java
+++ Course/05_conditionals/jc_05_03_ternary.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class jc_05_03_ternary {
 
   public static void ternary() {
-    String str = true ? "true" : "false";
-    System.out.println(str);
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter 0 or 1 : ");
+    int num = sc.nextInt();
+    num = num >= 1 ? 1 : 0;
+    
+    String str = num == 1 ? "true" : "false";
+    System.out.println(num + str);
+
+    sc.close();
   }
 
   public static void main(String[] args) {
